2e5de9b5961f08065a4b4691054db136c0ee676d
Ashutosh5333 <lakshakarashutosh@gmail.com>
2023-03-28 10:00:19 +0530

    spiwer added

---
+++ data.js
@@ -0,0 +1,25 @@
+
+export const data = [
+      {
+         image:"https://celebrare.in/wedding-card/sikh.html"  
+      },
+      {
+        image:"https://celebrare.in/wedding-card/hindu.html"  
+      },
+      {
+        image:"https://celebrare.in/wedding-card/south-indian.html"  
+      },
+      {
+        image:"https://celebrare.in/wedding-card/muslim.html"  
+      },
+      {
+        image:"https://celebrare.in/wedding-card/marathi.html"  
+      },
+      {
+        image:"https://celebrare.in/wedding-card/sikh.html"  
+      },
+      {
+        image:"https://celebrare.in/wedding-card/sikh.html"  
+      },
+      
+]
